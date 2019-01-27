200ab02cad9818601f20248f691ac3ee2b9c9c8e
Hugo Arganda <hugo.arganda.ext@valeo.com>
2019-01-27 09:23:11 -0600

    Cleaning Object files

--- TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/btn_long.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/btn_long.o: \
- TouchGFX/generated/images/src/btn_long.cpp \
- Middlewares/ST/TouchGFX/touchgfx//framework/include/touchgfx/hal/Config.hpp
-
-Middlewares/ST/TouchGFX/touchgfx//framework/include/touchgfx/hal/Config.hpp:
--- TouchGFX/build/STM32F746-DISCO/TouchGFX/target/GPIO.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F746-DISCO/TouchGFX/target/GPIO.o: \
- TouchGFX/target/GPIO.cpp \
- Middlewares/ST/TouchGFX/touchgfx//framework/include/touchgfx/hal/GPIO.hpp
-
-Middlewares/ST/TouchGFX/touchgfx//framework/include/touchgfx/hal/GPIO.hpp:
